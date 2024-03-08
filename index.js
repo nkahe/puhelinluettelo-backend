46287df9c300d0c7af5653800ffc8a3ea846c8c1
Henri K <12832691+nkahe@users.noreply.github.com>
2024-03-08 10:54:23 +0200

    eka commit. kaikkien ja yksittäisten henkilöiden listaus, infon haku, henkilön poisto.

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-532232",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+];
+
+app.get('/api/persons', (req, res) => {
+  console.log('/api/persons tuli pyyntö');
+  res.json(persons);
+});
+
+
+app.get('/info', (req, res) => {
+  const timeStamp = new Date();
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${timeStamp}</p>
+  `);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    persons = persons.filter(person => person.id !== id);
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+  
+});
+
+const PORT = 3004;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
