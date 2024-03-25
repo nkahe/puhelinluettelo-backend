4c1f41a3d93e11165a342f5cc2b581cd22bd8791
Henri K <12832691+nkahe@users.noreply.github.com>
2024-03-25 12:47:38 +0200

    dotenv, numeroiden haku mongoDB:stä

---
+++ models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+
+console.log('connecting to', url);
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url)
+  .then(res => {
+    console.log('connected to MongoDB');
+  }).catch(err => {
+    if (err) {
+      console.log('error connecting to MongoDB:', error.message);
+    } else {
+      console.log('Failed to connect to database.');
+    }
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+// .id laitetaan id ja turha __V pois.
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Person', personSchema);
