a769ead5918eaaacfe9fb7c03bc897f901dd9ac0
Henri K <12832691+nkahe@users.noreply.github.com>
2024-04-10 12:06:02 +0300

    POST virheenkäsittely

--- index.js
+++ index.js
@@ -138,7 +138,8 @@ app.post('/api/persons', (req, res, next) => {
         // Menee catchiin jos validointi epäonnistuu.
         newPerson.save().then(savedPerson => {
           res.json(savedPerson);
-        }).catch(err );
+        })
+        .catch(error => next(error));
       }
     }).catch(error => next(error))
 });
@@ -149,7 +150,7 @@ const errorHandler = (error, req, res, next) => {
   }
   if (res.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id'});
-  } else if (error.name === 'ValidationError') {
+  } else if (error.name && error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message });
   }
 
